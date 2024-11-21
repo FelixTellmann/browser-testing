b286c1bf196a01b223f8bb0f5e8dbc85a1741633
Felix Tellmann <felix@tellmann.co.za>
2024-11-21 11:44:11 +0200

    Browser Testing

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -2,8 +2,13 @@ import { test } from '@playwright/test'
 
 test('test browser', async ({ page }) => {
   // point this to wherever you want
-  await page.goto('http://localhost:3000/')
+  // await page.goto('http://localhost:3000/')
+  // page.goto('http://localhost:3000/')
+  page.goto('https://try.everymanjack.com/')
+  // await page.goto('https://www.houseofmacadamias.com/')
+  // await page.goto('https://hdlol13aq1m7sopm-19826381.shopifypreview.com')
 
   // keep browser open
   await page.pause()
 })
+
